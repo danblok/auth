1e8f77372c6fe890f74a7b78a43074170aadf107
danblok <danytokmakov@gmail.com>
2024-02-12 12:27:58 +0500

    feat: print available routes of HTTP API

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,19 +11,23 @@ import (
 )
 
 func main() {
-	key, err := os.ReadFile("jwt")
+	addr := flag.String("addr", ":3000", "listen addr of the server")
+	keyPath := flag.String("keypath", "/run/secrets/jwt_key", "key path of a signing jwt key")
+	flag.Parse()
+
+	key, err := os.ReadFile(*keyPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	svc := service.NewJWTService([]byte(key))
 	svc = logging.NewLoggingService(svc)
-	addr := flag.String("addr", ":3000", "listen addr of the server")
-
-	flag.Parse()
 
 	srv := api.NewHTTPService(svc, *addr)
-	log.Printf("started server on port: %s", *addr)
+	log.Printf("started server on http://localhost%s\n", *addr)
+	log.Printf(`available routes:
+	receive token: POST http://localhost%s/token
+	validate token: GET http://localhost%s/validate?token=<your_token>`, *addr, *addr)
 	if err := srv.Run(); err != nil {
 		log.Fatal("couldn't start the server")
 	}
